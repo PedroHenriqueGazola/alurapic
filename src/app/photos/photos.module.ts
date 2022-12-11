4bfda958ab86756e7bce0260c0c3166e932b643d
Pedro Gazola <ph-gazola@hotmail.com>
2022-12-10 23:35:02 -0300

    Criaçao de um pipe

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,13 +5,15 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { CommonModule } from "@angular/common";
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescriptionPipe
     ],
     exports: [
         PhotoComponent

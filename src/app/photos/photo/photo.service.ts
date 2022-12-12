ba6a9f06c6f4688a482116011711138d2631a205
Pedro Henrique Gazola <ph-gazola@hotmail.com>
2022-12-12 09:14:56 -0300

    Paginaçao

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
@@ -9,5 +9,10 @@ export class PhotoService {
 
     listFromUser(username: string) {
         return this.http.get<Photo[]>(`http://localhost:3000/${username}/photos`)
+    }
+
+    listFromUserPaginated(username: string, page: number) {
+        const params = new HttpParams().append('page', page)
+        return this.http.get<Photo[]>(`http://localhost:3000/${username}/photos`, {params})
     }
 }
